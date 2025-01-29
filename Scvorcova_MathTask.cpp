aef03a31a6b3957371bf365afd174403d275d612
poliskvor <poli290799@mail.ru>
2025-01-29 13:25:03 +0300

    Start project

---
+++ Scvorcova_MathTask.cpp
@@ -0,0 +1,51 @@
+/* - ввести объем информации в байтах B
+- выразить объем B в мегабайтах
+- выразить объем B в гигабайтах */
+#include <iostream>
+#include <cmath> 
+#include "Scvorcova_MathTask.h"
+using namespace std;
+double B;
+
+int enterVolume(){
+    string raw_input;
+    getline(cin, raw_input);
+    while (!UserInput(raw_input)){
+        getline(cin, raw_input);
+    }
+    B = stoi(raw_input);
+}
+
+int main() {
+    int choice;
+
+    do {
+        cout << "Меню:\n";
+        cout << "1. Ввести объем информации в байтах B\n";
+        cout << "2. Выразить объем B в мегабайтах\n";
+        cout << "3. Выразить объем B в гигабайтах\n";
+        cout << "0. Выход\n";
+        cout << "Выберите пункт меню: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                cout << "Введите объём информации в байтах: " << endl;
+                enterVolume();
+                break;
+            case 2:
+                cout << "Выразить объем B в мегабайтах: " << convertToMegabytes(B) << endl;
+                break;
+            case 3:
+                cout << "Выразить объем B в гигабайтах: " <<  convertToGigabytes(B) << endl;
+                break;
+            case 0:
+                cout << "Выход из программы." << endl;
+                break;
+            default:
+                cout << "Ошибка. Попробуйте снова." << endl;
+        }
+    } while (choice != 0);
+
+    return 0;
+}
---
+++ Scvorcova_MathTask.h
@@ -0,0 +1,24 @@
+#include <cstring>
+#include <cmath>
+using namespace std;
+
+
+double convertToMegabytes(double B) {
+    return B / 1024;
+}
+
+double convertToGigabytes(double B) {
+    return B / 1024 / 1024;
+}
+
+bool UserInput(string input) {
+    if (input.empty()) return false;
+    try {
+        int number = stoi(input);
+    }
+    catch (...) //если возникла ошибка в блоке try
+    { return false; }
+    return true;
+}
+
+#endif
